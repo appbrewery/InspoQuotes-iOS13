39deb7717360c39c46fce5c34948f127a17e61a7
Philipp <info@londonappbrewery.com>
2019-11-08 12:31:13 +0000

    Initial Commit

---
+++ InspoQuotes/QuoteTableViewController.swift
@@ -0,0 +1,117 @@
+//
+//  QuoteTableViewController.swift
+//  InspoQuotes
+//
+//  Created by Angela Yu on 18/08/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class QuoteTableViewController: UITableViewController {
+    
+    var quotesToShow = [
+        "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
+        "All our dreams can come true, if we have the courage to pursue them. – Walt Disney",
+        "It does not matter how slowly you go as long as you do not stop. – Confucius",
+        "Everything you’ve ever wanted is on the other side of fear. — George Addair",
+        "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",
+        "Hardships often prepare ordinary people for an extraordinary destiny. – C.S. Lewis"
+    ]
+    
+    let premiumQuotes = [
+        "Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine. ― Roy T. Bennett",
+        "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear. – Nelson Mandela",
+        "There is only one thing that makes a dream impossible to achieve: the fear of failure. ― Paulo Coelho",
+        "It’s not whether you get knocked down. It’s whether you get up. – Vince Lombardi",
+        "Your true success in life begins only when you make the commitment to become excellent at what you do. — Brian Tracy",
+        "Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going. – Chantal Sutherland"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    
+    
+    
+    @IBAction func restorePressed(_ sender: UIBarButtonItem) {
+        
+    }
+
+
+}
